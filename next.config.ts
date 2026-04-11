0a6c53e0aaacb43411a0be723ffd46b41768f1ee
Caesar <caesar@onbrd.net>
2026-04-11 18:36:57 -0400

    Fix CSP: allow YouTube iframes in frame-src

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
               "img-src 'self' data: blob: https:",
               "font-src 'self'",
               "connect-src 'self' https://*.supabase.co https://api.stripe.com https://www.facebook.com",
-              "frame-src https://js.stripe.com https://hooks.stripe.com",
+              "frame-src https://js.stripe.com https://hooks.stripe.com https://www.youtube.com https://www.youtube-nocookie.com",
               "object-src 'none'",
               "base-uri 'self'",
               "form-action 'self'",
